bc84ac76b48bdf26ba829266524d3610cb3f44c4
burakesen <eeburakesen@gmail.com>
2025-09-01 10:10:28 +0300

    feat: recipe detail routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'  // <- bu satır olmalı
+import RecipeDetail from './recipe-detail.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/recipe/:slug" element={<RecipeDetail />} />
+    </Routes>
+  </BrowserRouter>
+)
